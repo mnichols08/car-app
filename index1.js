bc725316d48f99fa5eacb9c97a73eba7f04c9b24
Mikey Nichols <33106391+mnichols08@users.noreply.github.com>
2020-01-15 22:33:44 -0500

    Create index1.js

---
+++ index1.js
@@ -0,0 +1,24 @@
+// just for fun I am writing a program that will allow me to input parts
+// from my car along with any hardware it may require for install
+
+class Car {
+ this.make = Eagle // user inputs somehow
+ this.model = Talon
+ this.year = 1990
+ this.trim = TSi
+ this.color = Blue
+ this.horsepower = 195 // lets make this a function based upon the components of the vehicle
+ this.parts = [
+  bumpers, fenders, wipers, antenna, fuel cap, hood, hatch, sun roof, doors, side panels, interior, headlights, grill, hood latch
+ ]
+}
+
+class Catalog {
+ this.parts = []
+ 
+}
+
+class Part extends Category{
+  super()
+  this.
+}
